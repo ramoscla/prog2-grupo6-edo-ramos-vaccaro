763e2494f3d9c8ed95752d239579fdb9155ec4bb
joaquinaedo <jedo@udesa.edu.ar>
2024-04-15 17:29:55 -0300

    Hice la mitad de la base de datos

---
+++ db/modulo.js
@@ -0,0 +1,109 @@
+const datos = {
+    listaProductos: [{
+        nombre: "Pistacho salado con cáscara",
+        foto: <img src="'https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/p/i/pistacho-con-c_scara-salado-y-tostado-x-300-gr.png" ></img>,
+        comentarios: [{
+            nombreUsuario: "Carlos",
+            texto: "Muy ricos.",
+            fotoPerfil: "Carlos"
+        },
+        {
+            nombreUsuario: "Juan",
+            texto: "Bastante saladitos.",
+            fotoPerfil: "juan"
+
+        },
+        {
+            nombreUsuario: "Ana",
+            texto: "Muy nutritivos.",
+            fotoPerfil: "ana"
+
+        }],
+        descripcion: 'Ideal como snack. Viene con cáscara y cuenta con una textura crocante y sabor salado. No recomendado para hipertensos'
+
+    },
+    {
+        nombre: "Nuez pelada mariposa",
+        foto: "nuez",
+        comentarios: [{
+            nombreUsuario: "Carlos",
+            texto: "Muy ricos.",
+            fotoPerfil: "Carlos"
+        },
+        {
+            nombreUsuario: "Juan",
+            texto: "Bastante saladitos.",
+            fotoPerfil: "juan"
+        },
+        {
+            nombreUsuario: "Ana",
+            texto: "Muy nutritivos.",
+            fotoPerfil: "ana"
+
+        }],
+        descripcion: 'Variedad de nuez entera, destacada para aprovechar sus aceites naturales cardioprotectores. De sabor y textura suave.'
+    },
+    {
+        nombre: "Pistacho  salado con cáscara",
+        foto: <img src="'https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/p/i/pistacho-con-c_scara-salado-y-tostado-x-300-gr.png" ></img>,
+        comentarios: [{
+            nombreUsuario: "Carlos",
+            texto: "Muy ricos.",
+            fotoPerfil: "Carlos"
+        },
+        {
+            nombreUsuario: "Juan",
+            texto: "Bastante saladitos.",
+            fotoPerfil: "juan"
+
+        },
+        {
+            nombreUsuario: "Ana",
+            texto: "Muy nutritivos.",
+            fotoPerfil: "ana"
+
+        }],
+        descripcion: 'Ideal como snack. Viene con cáscara y cuenta con una textura crocante y sabor salado. No recomendado para hipertensos'
+    },
+    {
+        nombre: "Nuez entera extra light",
+    foto: '<img src="https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/n/u/nuez-pelada-mariposa-extra-light-300-gr.jpg"></img>',
+    comentarios: [
+        {
+            nombreUsuario: "Carlos",
+            texto: "Excelente para ensaladas.",
+            fotoPerfil: "Carlos"
+        },
+        {
+            nombreUsuario: "Maria",
+            texto: "Me encanta con yogur.",
+            fotoPerfil: "maria"
+        },
+        {
+            nombreUsuario:"Ana", 
+            texto: "Muy nutritivos", 
+            fotoPerfil: "ana"
+        }
+    ],
+    descripcion: 'Variedad de nuez entera, destacada para aprovechar sus aceites naturales cardioprotectores. De sabor y textura suave.'
+},
+{
+    nombre: "Castañas de cajú tostadas con sal",
+    foto: '<img src="https://cdn.newgarden.com.ar/media/catalog/product/cache/dda7253a1a2f6711745de410175d10f8/c/a/castana-de-caju-tostada-con-sal-x-350-gr-sin-tacc_1.jpg"></img>',
+    comentarios: [
+        {
+            nombreUsuario: "Luis",
+            texto: "Perfectas para picar entre horas.",
+            fotoPerfil: "luis"
+        },
+        {
+            nombreUsuario: "Ana",
+            texto: "Un poco saladas para mi gusto.",
+            fotoPerfil: "ana"
+        }
+    ],
+    descripcion: 'Gustosas castañas de cajú tostadas con sal. Ideal para picar entre horas.'}],
+
+    listaUsuarios: [{}],
+
+}
