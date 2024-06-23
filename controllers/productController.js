b54e8fd024e37ea2d1be13cabcc80746d0d1eab4
Natalia Vaccaro <nvaccarobianchi@udesa.edu.ar>
2024-06-23 19:54:36 -0300

    volvi a poner product edit/delete/store x merge fallido

--- controllers/productController.js
+++ controllers/productController.js
@@ -38,6 +38,79 @@ const productController = {
             return res.render('product', { errors: errors.mapped(), old: req.body});        
         }     
     },
+    productDelete: function (req, res) {
+      let objeto = req.params.id;
+      db.Producto.findByPk(objeto).then((resultados) => {
+        if (
+          req.session.user != undefined &&
+          req.session.user.id == resultados.usuarioId
+        ) {
+          console.log(req.session.user.id);
+          db.Comentario.destroy({
+            where: {
+              productoId: objeto,
+            },
+          }).then(() => {
+            db.Producto.destroy({
+              where: {
+                id: objeto,
+              },
+            }).then((resultados) => {
+              return res.redirect("/");
+            });
+          });
+        } else {
+          return res.redirect("/users/login");
+        }
+      });
+    },
+    productEdit: function (req, res) {
+      let objeto = req.params.id;
+  
+      if (req.session.user != undefined) {
+        db.Producto.findByPk(objeto, {
+          include: [{ association: "usuario" }],
+        }).then((resultados) => {
+          if (
+            req.session.user != undefined &&
+            req.session.user.id == resultados.usuarioId
+          ) {
+            res.render("product-edit", {
+              productos: resultados,
+              usuario: req.session.user,
+            });
+          }
+        });
+      } else {
+        return res.redirect("/users/login");
+      }
+    },
+    productEditStore: function (req, res) {
+      let form = req.body;
+      let errors = validationResult(req);
+  
+  
+  if (errors.isEmpty()) {
+      db.Producto.update(
+        {
+          nombre: form.nombre,
+          foto: form.foto,
+          descripcion: form.descripcion,
+        },
+        {
+          where: {
+            id: form.id,
+          },
+        }
+      )
+      .then(function (resultados) {
+        return res.redirect("/");    
+      })
+      .catch(function (errors) {
+        console.log(errors);
+      });
+    }
+  },
     productComentario: function(req,res){
         let form = req.body;
         let errors = validationResult(req)
