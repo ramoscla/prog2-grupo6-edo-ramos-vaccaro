d7f34cced69f00e3c39664a1705742f71713bd32
joaquinaedo <jedo@udesa.edu.ar>
2024-06-23 19:57:21 -0300

    ...

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,28 +1,25 @@
-const db = require('../database/models');
-const { validationResult } = require('express-validator'); 
+const db = require("../database/models");
+const { validationResult } = require("express-validator");
 
 const productController = {
-    product: function (req, res) {
-        let objeto = req.params.id;
-        console.log(objeto);
-        db.Producto.findByPk (objeto,
-            {include : [{association: 'usuario'}]}
-        )
-        .then( (resultados) => {
-            res.render('product', { productos: resultados  });
-            console.log(resultados);
-        })
-    },
-    productAdd: function (req, res) {
-        if (req.session.user != undefined) {
-            return res.render('product-add', { usuario: req.session.user });
-        } else {
-            return res.redirect('/users/login');
-        }
-    },
-    productStore: function (req, res) {
-        let form = req.body;
-        let errors = validationResult(req)
+  product: function (req, res) {
+    let objeto = req.params.id;
+    db.Producto.findByPk(objeto, {
+      include: [{ association: "usuario" }],
+    }).then((resultados) => {
+      res.render("product", { productos: resultados });
+    });
+  },
+  productAdd: function (req, res) {
+    if (req.session.user != undefined) {
+      return res.render("product-add", { usuario: req.session.user });
+    } else {
+      return res.redirect("/users/login");
+    }
+  },
+  productStore: function (req, res) {
+    let form = req.body;
+    let errors = validationResult(req);
 
         if (errors.isEmpty()) {
 
@@ -37,39 +34,7 @@ const productController = {
         }  else {
             return res.render('product', { errors: errors.mapped(), old: req.body});        
         }     
-    },
-    productComentario: function(req,res){
-        let form = req.body;
-        let errors = validationResult(req)
-
-        if (errors.isEmpty()) {
-
-            if(req.session.user == undefined ){
-             return res.redirect('/users/login ')
-            } else {
-                     
-                        let comentario = {
-                            id_usuario: req.session.user.id,
-                            id_producto: req.params.id,
-                            texto: form.comentario
-                        }
-
-                        db.Comentario.create(comentario)
-                        .then((result) => {
-                            return res.redirect("/product/id/" + req.params.id);
-                        }).catch((err) => {
-                            console.log(err);
-                            return res.redirect("/product/id/" + req.params.id);
-                        })
-       
-            
-            console.log(comentario)
-        }
-  
     }
-}
 }
 
 module.exports = productController;
-
-// TEST COMIT X CAMBIO DE COMPUTADORA
